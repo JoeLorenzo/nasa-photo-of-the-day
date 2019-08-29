44b8eecc41a124417a9c37256898b64c6eba59f6
JoeLorenzo <Joselmedrano@gmail.com>
2019-08-28 21:45:06 -0500

    -mvp met

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom"
 import "./App.css";
 import axios from "axios"
@@ -8,8 +8,9 @@ function App() {
   const [title, setTitle] = useState("")
   const [url, setUrl] = useState("")
   const [explanation, setExplanation] = useState("")
-
+  const [date, setDate] = useState("")
   console.log("top")
+  useEffect(() => {
   axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14")
   .then(res => {
     console.log(res.data)
@@ -17,11 +18,14 @@ function App() {
     setTitle(res.data.title)
     setExplanation(res.data.explanation)
     setUrl(res.data.url)
+    setDate(res.data.date)
   })
+}, [])
   return (
     <div className="App">
       <p>
         <NasaCards
+          date={date}
           title={title}
           url={url}
           explanation={explanation}
--- src/components/NasaCards.js
+++ src/components/NasaCards.js
@@ -5,6 +5,7 @@ function NasaCards(props){
   return (
     <div>
       <h2> {props.title} </h2>
+      <h3> {props.date} </h3>
       <img src={props.url} />
       <p> {props.explanation} </p>
     </div>
